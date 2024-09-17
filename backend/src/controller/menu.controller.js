9ea2b2c6675fd7019ed5531ef0a1bcf54e1f66be
Ashish9738 <ayadav9738@gmail.com>
2024-09-17 15:23:26 +0530

    Added the validation of itemName Repetation

--- backend/src/controller/menu.controller.js
+++ backend/src/controller/menu.controller.js
@@ -1,19 +1,12 @@
 // This menu controller is for shop owner
-
 import { Menu } from "../models/menu.models.js";
 import { Shop } from "../models/shop.model.js";
 
 export const createMenu = async (req, res) => {
   try {
-    const {
-      itemName,
-      itemDescription,
-      price,
-      category,
-      picture,
-      shopId,
-      offerId,
-    } = req.body;
+    const shopId = req.params.shopId;
+    const { itemName, itemDescription, price, category, picture, offerId } =
+      req.body;
     const menu = new Menu({
       itemName,
       itemDescription,
@@ -29,6 +22,18 @@ export const createMenu = async (req, res) => {
     if (!checkForShop) {
       return res.status(404).json({
         message: "Shop doesn't exist",
+        error: error.message,
+      });
+    }
+
+    const checkForItemNameExist = await Menu.findOne({
+      shopId,
+      itemName,
+    });
+
+    if (checkForItemNameExist) {
+      return res.status(400).json({
+        message: "Item with the name already exist",
         error: error.message,
       });
     }
@@ -60,6 +65,15 @@ export const getMenu = async (req, res) => {
 export const getAllMenuOfShop = async (req, res) => {
   try {
     const shopId = req.params.shopId;
+    const checkForShop = await Shop.findById(shopId);
+
+    if (!checkForShop) {
+      return res.status(404).json({
+        message: "Shop doesn't exist",
+        error: error.message,
+      });
+    }
+
     const menu = await Menu.find({
       shopId,
     });
@@ -75,6 +89,29 @@ export const getAllMenuOfShop = async (req, res) => {
 export const updateMenu = async (req, res) => {
   try {
     const menuId = req.params.id;
+    const shopId = req.params.shopId;
+    const checkForShop = await Shop.findById(shopId);
+
+    if (!checkForShop) {
+      return res.status(404).json({
+        message: "Shop doesn't exist",
+        error: error.message,
+      });
+    }
+
+    const { itemName } = req.body;
+    const checkForItemNameExist = await Menu.findOne({
+      shopId,
+      itemName,
+    });
+
+    if (checkForItemNameExist) {
+      return res.status(400).json({
+        message: "Item with the name already exist",
+        error: error.message,
+      });
+    }
+
     const updatedMenu = await Menu.findByIdAndUpdate(
       menuId,
       {
