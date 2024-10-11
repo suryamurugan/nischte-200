26b445a7a16afd3845fa5555de48586869eeb037
Ashish9738 <ayadav9738@gmail.com>
2024-10-11 14:57:52 +0530

    chore: added carousel

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,22 @@
 import { Navbar } from "../components/Navbar";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+
 import HeroImage from "../assets/HeroImage.png";
+import HeroImage2 from "../assets/HeroImage2.jpg";
+import HeroImage3 from "../assets/HeroImage3.jpg";
+import HeroImage4 from "../assets/HeroImage4.jpg";
+
+const HeroImages = [
+  { name: "Banner1", path: HeroImage, id: 1 },
+  { name: "Banner2", path: HeroImage2, id: 2 },
+  { name: "Banner3", path: HeroImage3, id: 3 },
+  { name: "Banner4", path: HeroImage4, id: 4 },
+];
 
 export const Home = () => {
   return (
@@ -7,12 +24,26 @@ export const Home = () => {
       <div className="px-6 md:px-[200px]">
         <Navbar />
         {/* Hero section */}
-        <div className="h-[300px] w-full relative overflow-hidden">
-          <img
-            src={HeroImage}
-            alt="Hero Image"
-            className="w-full h-auto max-h-[500px] object-cover mt-3"
-          />
+        <div className="h-[500px] w-full relative overflow-hidden">
+          <Carousel
+            plugins={[
+              Autoplay({
+                delay: 3000,
+              }),
+            ]}
+          >
+            <CarouselContent>
+              {HeroImages &&
+                HeroImages.length > 0 &&
+                HeroImages.map(({ name, id, path }) => {
+                  return (
+                    <CarouselItem key={id}>
+                      <img src={path} alt={name} key={id} />
+                    </CarouselItem>
+                  );
+                })}
+            </CarouselContent>
+          </Carousel>
         </div>
       </div>
     </>
